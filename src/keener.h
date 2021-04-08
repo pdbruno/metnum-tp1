6105edff034ed68890cd247f6bd295213bd27a82
msorondo <msorondo@live.com.ar>
2021-04-08 14:09:47 -0300

    + declaraciones en .h

--- src/keener.h
+++ src/keener.h
@@ -6,3 +6,7 @@
 using namespace std;
 
 vector<double> keener(uint T, uint P, ifstream &inputFile);
+
+double h(double x, bool jugaron);
+double signo(double num);
+double g(const double Sij,const double Sji);
