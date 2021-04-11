e056c8d3b489425ac9b6e9d1e2491aa1e747515b
msorondo <msorondo@live.com.ar>
2021-04-11 19:03:39 -0300

    construccion de Keener arreglada

--- src/keener.cpp
+++ src/keener.cpp
@@ -4,10 +4,12 @@
 RECORDAR CITAS (EN DESARROLLO)>
 https://es.wikipedia.org/wiki/M%C3%A9todo_de_las_potencias
 
+Paper 151-2008
+Generalizing Google’s PageRank to Rank National Football League Teams
+
 */
 vector<double> keener(uint T, uint P, ifstream &inputFile){
     vector<vector<double>> K(T, vector<double>(T,0)); //keener matrix
-    vector<double> rd(T,0);// vector r derecho
     vector<double> rank(T,0);// vector r izquierdo
     vector<vector<double>> S(T, vector<double>(T,0)); // matriz de puntos anotados por i contra j
     vector<vector<bool>> J(T, vector<bool>(T, false)); // Jij=true si i y j jugaron
@@ -19,18 +21,24 @@ vector<double> keener(uint T, uint P, ifstream &inputFile){
     while (getline(inputFile, line,'\n')) {
         stringstream ss(line);
         int date, team1, points1, team2, points2, winner, loser, wpoints, lpoints;
-
         ss >> date >> team1 >> points1 >> team2 >> points2;
 
         if (points1 > points2) {
             winner = team1-1;
             loser = team2-1;
+
+            wpoints = points1;
+            lpoints = points2;
         }
         else {
             winner = team2-1;
             loser = team1-1;
+
+            wpoints = points2;
+            lpoints = points1;
         }
         
+
         J[winner][loser] = true;
         J[loser][winner] = true;
 
@@ -39,7 +47,9 @@ vector<double> keener(uint T, uint P, ifstream &inputFile){
         // recordar que los Sij y los Sji son la suma de los goles a lo largo de los partidos
 
     }
-
+    cout<<"----------------Matriz de Scores: --------------\n";
+    print_matrix(S);
+    cout<<"---------------------------------\n";
     for(int i = 0 ; i < T ; i++){
         for(int j = 0 ; j< T ; j++){
             K[i][j] = h(   g(   S[i][j], S[j][i]   ), J[i][j]   );
@@ -61,7 +71,7 @@ double h(double x, bool jugaron){
     // El efecto del puntaje en el ranking es cada vez menor al aumentar
     double res;
     if(jugaron)
-        res = 0.5 + 0.5 * sqrt(abs(2 * x -1) * signo(x - 0.5));
+        res = 0.5 + 0.5 *signo(x - 0.5)* sqrt(abs((2 * x) -1)     );
     else
         res = 0;    
     return res;
